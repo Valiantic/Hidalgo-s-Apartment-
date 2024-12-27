65884dc8101c759a70e3017285b5ba53d5a36ed6
StevenMadali <stevenmadali17@gmail.com>
2024-12-27 18:13:51 +0800

    Add Sidebar on Today's Due Date Calendar

--- admin/due-date.php
+++ admin/due-date.php
@@ -8,6 +8,9 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] != 'admin') {
 
 include '../connections.php';
 
+$current_page = basename($_SERVER['PHP_SELF']); 
+
+
 // Fetch only the latest transaction date for each unit
 function fetchTransactionDates() {
     global $pdo;
@@ -46,6 +49,228 @@ function fetchTransactionDates() {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
     <style>
+            *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: sans-serif;
+        }
+        
+        body{
+            height: 100vh;
+            width: 100vw;
+            background-color: rgb(102, 153, 255) !important;
+            display: flex;
+            flex-direction: column;
+        }
+        
+        .menu{
+            display: flex;
+            flex-grow: 1;
+            overflow: hidden;
+        }
+        
+        .sidebar{
+            height: 100vh;
+            width: 60px;
+            background: #C6E7FF;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-evenly;
+            transition: all 0.5s ease;
+            
+        }
+        
+        .mainHead{
+            margin-left: 15px;
+            
+        }
+        
+        img{
+            height: 40px;
+            width: 40px;
+            border-radius: 50%;
+            margin-left: 10px;
+        }
+        
+        .items{
+            display: flex;
+            align-items: center;
+            font-size: 1.3rem;
+            color: #252525;
+            margin-left: 0px;
+            padding: 10px 0px;
+            font-family: 'Poppins', 'sans-serif';
+            font-size: 17px;
+            font-weight: 500;
+        }
+        
+        .sidebar li{
+            margin-left: 10px;
+            border-top-left-radius: 10px;
+            border-bottom-left-radius: 10px;
+        }
+        
+        .items i{
+            margin: 0 10px;
+        }
+        
+        .para{
+            opacity: 0;
+            transition: opacity 0.5s ease;
+        }
+        
+        .sidebar li:not(.logout-btn):hover {
+            background: #000;
+            color: #ffffff;    
+        }
+        
+        .logout-btn{
+            margin-top: 30px;
+            color: #B70202;
+        }
+        
+        .logout-btn:hover{
+            background-color: #B70202;
+            color: #ffffff;    
+        }
+        
+        .toggler{
+            position: absolute;
+            top: 0;
+            left: 0px;
+            padding: 10px 1px;
+            font-size: 1.4rem;
+            transition: all 0.5s ease;
+        }
+        
+        .toggler #toggle-cross {
+            display: none;
+        }
+        
+        .active.toggler #toggle-cross {
+            display: block;
+        }
+        
+        .active.toggler #toggle-bars {
+            display: none;
+        }
+        
+        .active.toggler {
+            left: 190px;
+        }
+        
+        .active.sidebar {
+            width: 220px;
+        }
+        
+        .active.sidebar .para{
+            opacity: 1;
+        }
+        
+        .content {
+            flex-grow: 1;
+            padding: 20px;
+            overflow-y: auto;
+        }
+        
+        a {
+            text-decoration: none;
+            color: inherit;
+        }
+        
+
+        /* CARD STYLING */
+        .btn-ocean {
+            background-color: #4DA1A9;
+            color: #ffffff;
+        }
+
+        .btn-ocean:hover {
+            background-color:rgb(125, 187, 205);
+            color: #ffffff;
+        }
+        
+        .height-img {
+        max-height: 220px; 
+        width: auto;
+        }
+        .card-img-top {
+        margin-top: 10px;
+        width: 100%;
+        height: auto;
+        object-fit: contain; 
+        border-radius: 0; 
+        }
+
+        .card-text{
+            font-family: 'Poppins', 'sans-serif';
+            font-size: 20px;
+            font-weight: 700;
+        }
+        .card-title{
+            font-family: 'Poppins', 'sans-serif';
+            font-size: 30px;
+            font-weight: 500;
+        }
+        a{
+            font-family: 'Poppins', 'sans-serif';
+            font-size: 17px;
+            font-weight: 300;
+        }
+        h4, h5 {
+            font-family: 'Poppins', 'sans-serif';
+            font-size: 20px;
+            font-weight: 500;
+        }
+        .custom-btn-font {
+        font-size: 1.35rem; /* Adjust the size as needed */
+        }
+
+        /* MEDIA QUERIES */
+
+        /* // FOR TABLET AND MOBILE VIEW */
+        @media (max-width: 768px) {
+            .sidebar{
+            height: 100vh;
+            width: 70px;
+            background: #C6E7FF;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-evenly;
+            transition: all 0.5s ease;
+        }
+
+        .active.toggler {
+            left: 123px;
+        }
+        
+        .active.sidebar {
+            width: 225px;
+        }
+
+        .items{
+            display: flex;
+            align-items: center;
+            font-size: 1.3rem;
+            color: #252525;
+            margin-left: 0px;
+            margin-right: 10px;
+            padding: 10px 0px;
+            font-family: 'Poppins', 'sans-serif';
+            font-size: 17px;
+            font-weight: 500;
+        }
+        }
+
+        .sidebar .active-menu {
+            background: black;
+            color: white;
+        }
+        .sidebar .active-menu a {
+            color: white;
+        }
+
         .calendar-day {
             min-height: 120px;
             background: #fff;
@@ -82,13 +307,59 @@ function fetchTransactionDates() {
         }
     </style>
 </head>
-<body>
+<body class="bg-light">
 
-<div class="container-fluid py-4">
-    <div class="row mb-4 calendar-header">
-        <div class="col">
-        <a href="dashboard.php"><button class="btn btn-primary mx-2">Back</button></a>
+<div class="menu">
+    <div class="sidebar">
+        <div class="logo items">
+            <span class="mainHead para">
+                <h5>Hidalgo's</h5>
+                <h4>Apartment</h4>
+            </span>
         </div>
+
+        <li class="items  <?php echo $current_page == 'due-date.php' ? 'active-menu' : ''; ?>">
+            <a href="dashboard.php"><i class="fa-solid fa-chart-simple"></i></a>
+            <p class="para"><a href="dashboard.php">Dashboard</a></p>
+        </li>
+
+        <li class="items <?php echo $current_page == 'units.php' ? 'active-menu' : ''; ?>">
+            <a href="units.php"><i class="fa-solid fa-home"></i></i></a>
+            <p class="para"><a href="units.php">Units</a></p>
+        </li>
+
+        <li class="items <?php echo $current_page == 'tenants.php' ? 'active-menu' : ''; ?>">
+            <a href="tenants.php"> <i class="fa-solid fa-user"></i></a>
+            <p class="para"><a href="tenants.php">Tenants</a></p>
+        </li>
+        <li class="items <?php echo $current_page == 'message-admin.php' ? 'active-menu' : ''; ?>">
+            <a href="message-admin.php"> <i class="fa-solid fa-message"></i></a>
+            <p class="para"><a href="message-admin.php">Message</a></p>
+        </li>
+
+        <li class="items <?php echo $current_page == 'mail.php' ? 'active-menu' : ''; ?>">
+            <a href="mail.php"> <i class="fa-solid fa-envelope"></i></a>
+            <p class="para"><a href="mail.php">Mails</a></p>
+        </li>
+
+        <li class="items logout-btn">
+            <!-- ENCLOSED THE ANCHOR TAG WITHIN THE LIST ITEM -->
+            <a href="logout.php"> <i class="fa-solid fa-right-from-bracket"></i></a>
+            <p class="para"><a href="logout.php">Log-out</a></p>
+        </li>
+    </div>
+
+    <div class="toggler">
+        <i id="toggle-bars">
+            <img src="../assets/images/logov3.png" alt="">
+        </i>
+        <i class="fa-solid fa-xmark" id="toggle-cross"></i>
+    </div>
+
+    <div class="content">
+    <div class="container-fluid py-4">
+    <div class="row mb-4 calendar-header">
+       
         <div class="col text-center">
             <h2>Unit Deadlines</h2>
         </div>
@@ -122,6 +393,24 @@ function fetchTransactionDates() {
 
     <div id="calendar"></div>
 </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>    
+
+<script>
+    const toggler = document.querySelector('.toggler')
+    const sidebar = document.querySelector('.sidebar')
+
+    const showFull = () => {
+        toggler.addEventListener('click', ()=> {
+            toggler.classList.toggle('active')
+            sidebar.classList.toggle('active')
+        })
+    }
+
+
+    showFull()
+</script>
 
 <script>
 const transactionData = <?php echo fetchTransactionDates(); ?>;
